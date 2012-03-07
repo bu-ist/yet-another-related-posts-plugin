5d14409ce7fbf20dfbd10084214324aff31d5f37
Gregory Cornelius <gcorne@gmail.com>
2012-03-07 02:00:27 +0000

    Merge from vendor branch to upgrade YARRP to 3.5.1.

--- blogglue.php
+++ blogglue.php
@@ -1,58 +1,9 @@
 <?php
-function yarpp_show_blogglue_upsell() {
-  if (current_user_can('activate_plugins') &&
-			get_option( 'yarpp_blogglue_promo_runonce', false) !== false)
-		return;
-	update_option( 'yarpp_blogglue_promo_runonce', true );
-?>
-<style type="text/css">
-#TB_window { min-width: 670px; min-height: 370px; max-height: 370px; }
-#blogglue_top { color: black; padding: 15px; border-bottom: 1px solid #CCC; overflow:auto; }
-#blogglue_top img { float: left; border: 3px solid #555;margin-right: 15px; }
-#blogglue_top p { line-height: 1.7em; }
-#blogglue_bottom, #blogglue_bottom ul { text-align: center; }
-#blogglue_bottom h2 { font-family: "Copperplate Gothic Light"; font-size: 1.3em; }
-#blogglue_bottom a { display: block; width: 150px; padding: 7px 5px; color: white !important; text-decoration: none; font-size: 18px; margin: 0 auto; background-color: #88AF3D; border: 2px solid #5C5C5C; border-radius: 5px; -moz-border-radius: 5px; -webkit-border-radius: 5px; text-align: center; }
-#signature { float:right; }
-#disclaimer { color: #666; font-size: 10px; padding-bottom: 0.6em }
-p.button_info { margin: 10px auto; font-size: 10px; color: #666; font-weight: bold; width: 150px; }
-</style>
-<div id="blogglue_update" style="display: none;">
-<div id="blogglue_top">
-<img src="<?php echo plugins_url( 'mitcho-small.jpg', __FILE__ ) ; ?>" alt="mitcho's Photo"/>
-<h2>A Note From YARPP's Author, mitcho</h2>
-<p>For a long time YARPP has been great at linking posts within a single blog. BlogGlue gives your site Related Posts from across the BlogGlue Network and your posts will also show up on other sites you trust. <strong>BlogGlue is the next generation of YARPP.</strong>
-<span id="signature">&mdash; mitcho</span></p>
-</div>
-<div id="blogglue_bottom">
-<table>
-<tr>
-<td width="200">
-  <a href="<?php echo  wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=arkayne-site-to-site-related-content'), 'install-plugin_arkayne-site-to-site-related-content'); ?>">Get BlogGlue</a>
-  <p class="button_info">Get your posts promoted by other blogs today.</p>
-</td>
-<td width="250">
-<h2>Benefits Of BlogGlue</h2>
-<ul>
-	<li>Related Links on Partner Blogs</li>
-	<li>Related Links in Tweets</li>
-	<li>Off-server Processing</li>
-	<li>Built-in SEO Analyzer</li>
-</ul>
-</td>
-<td width="200">
-  <a href="#" onclick="tb_remove(); return false;">No Thanks</a>
-  <p class="button_info">I don't want other blogs promoting me.</p>
-</td>
-</tr>
-<tr><td colspan="3" id="disclaimer">Installing BlogGlue will deactivate YARPP. Your YARPP settings will be saved.<img src="http://www.blogglue.com/cohorts/track/yarpp_popup.gif"></td></tr>
-</table>
-</div>
-</div>
-<?php
-	echo '<script>window.onload=function(){ tb_show("BIG ANNOUNCEMENT: YARPP has partnered with BlogGlue to connect blogs.", "#TB_inline?height=600&width=650&inlineId=blogglue_update", null); }</script>';
+function yarpp_blogglue_enqueue() {
+	wp_enqueue_script( 'thickbox' );
+	wp_enqueue_style( 'thickbox' );
 }
-add_action('admin_print_scripts-plugins.php', 'yarpp_show_blogglue_upsell' );
+add_action( 'admin_enqueue_scripts', 'yarpp_blogglue_enqueue' );
 
 function add_yarpp_blogglue_meta_box() {
 	class YARPP_Meta_Box_BlogGlue extends YARPP_Meta_Box {
@@ -60,21 +11,20 @@ function add_yarpp_blogglue_meta_box() {
 			$pluginurl = plugin_dir_url(__FILE__);
 			?>
 <style type="text/css">
-#blogglue_upsell {
+#blogglue_upsell .center {
 	text-align: center;
 }
 #blogglue_upsell ul {
-	list-style-type: disc;
-	list-style-position: inside;
 	text-align: left;
 	margin: 10px 0 10px 15px;
 }
-#blogglue_install_steps {
-	text-align: center;
-	height: 200px;
+#blogglue_upsell ul li {
+	list-style: disc outside !important;
 }
 #TB_ajaxContent {
-	height: 220px !important;
+	height: 480px !important;
+	padding: 10px;
+	overflow: hidden;
 }
 ul.install_help {
 	list-style-type: disc;
@@ -84,31 +34,28 @@ ul.install_help {
 }
 </style>
 <div id="blogglue_upsell">
-	<img src="http://s3.amazonaws.com/arkayne-media/img/logo-md.png" alt="BlogGlue Logo"/>
+	<p class="center"><img src="http://s3.amazonaws.com/arkayne-media/img/logo-md.png" alt="BlogGlue Logo"/></p>
+	<p>In addition to related links on your own site, YARPP and BlogGlue have partnered to increase your blog’s audience by:</p>
 	<ul>
-		<li>Related Links on Partner Blogs</li>
-		<li>Related Links in Tweets</li>
-		<li>Off-server Processing</li>
-		<li>Built-in SEO Analyzer</li>
+		<li>delivering a new audience from trusted partners with relevant, crosslinked content</li>
+		<li>improving search engine placement</li>
+		<li>pulling traffic from Facebook, Twitter and LinkedIn</li>
 	</ul>
-	<a href="#TB_inline?height=300&width=400&inlineId=blogglue_install" class="thickbox"><img src="http://s3.amazonaws.com/arkayne-media/img/email_try.png" alt="Upgrade"/></a><img src="http://www.blogglue.com/cohorts/track/yarpp_sidebar.gif"/>
+	<p class="center"><a href="<?php echo  wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=arkayne-site-to-site-related-content'), 'install-plugin_arkayne-site-to-site-related-content'); ?>"><img src="http://s3.amazonaws.com/arkayne-media/img/installnow.png" alt="Install Now!"/></a><img src="http://www.blogglue.com/cohorts/track/yarpp_sidebar.gif"/></p>
+	
+	<p class="center">Want to learn more? <a href="#TB_inline?title=rar&height=450&width=610&inlineId=blogglue_video" title="Free Upgrade To BlogGlue: More Information" class="thickbox">Watch A Video</a></p>
 </div>
-<div id="blogglue_install" style="display: none;">
-	<div id="blogglue_install_steps">
+<div id="blogglue_video" style="display: none;">
 	<img src="http://s3.amazonaws.com/arkayne-media/img/logo.png" alt="BlogGlue Logo"/>
-	<ul class="install_help">
-		<li>Installing BlogGlue will disable YARPP</li>
-		<li>Your YARPP settings will still be saved</li>
-		<li>Once the download is complete, activate the BlogGlue plugin</li>
-		<li>After the plugin is activated, follow the signup instructions</li>
-	</ul>
-	<a href="<?php echo  wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=arkayne-site-to-site-related-content'), 'install-plugin_arkayne-site-to-site-related-content'); ?>"><img src="http://s3.amazonaws.com/arkayne-media/img/start_free_sm.png" width="94" height="30"/></a>
-	</div>
+
+	<iframe src="http://player.vimeo.com/video/33007489?title=0&amp;byline=0&amp;portrait=0&amp;color=ff6300" width="601" height="338" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+
+	<p style="text-align:center;"><a href="<?php echo  wp_nonce_url(self_admin_url('update.php?action=install-plugin&plugin=arkayne-site-to-site-related-content'), 'install-plugin_arkayne-site-to-site-related-content'); ?>"><img src="http://s3.amazonaws.com/arkayne-media/img/installnow.png" alt="Install Now!"/></a></p>
 </div>
 	<?php
 		}
 	}
 	
-	add_meta_box('yarpp_display_blogglue', 'Upgrade To BlogGlue', array(new YARPP_Meta_Box_BlogGlue, 'display'), 'settings_page_yarpp', 'side', 'core');
+	add_meta_box('yarpp_display_blogglue', 'Free Upgrade To BlogGlue', array(new YARPP_Meta_Box_BlogGlue, 'display'), 'settings_page_yarpp', 'side', 'core');
 }
 add_action( 'add_meta_boxes_settings_page_yarpp', 'add_yarpp_blogglue_meta_box' );
--- related-functions.php
+++ related-functions.php
@@ -1,29 +1,100 @@
 <?php
 
-// Here are the related_WHATEVER functions, as introduced in 1.1, which actually just use the yarpp_related and yarpp_related_exist functions.
+function yarpp_related($args = array(), $reference_ID = false, $echo = true) {
+	global $yarpp;
 
-// Since YARPP 2.1, these functions receive (optionally) one array argument. See the documentation for instructions on how to customize their output.
-
-function related_posts($a = array(),$echo=true,$reference_ID=false) {
-	return yarpp_related(array('post'),$a,$echo,$reference_ID);
+	if ( is_array($reference_ID) ) {
+		_doing_it_wrong( __FUNCTION__, "This YARPP function now takes \$args first and \$reference_ID second.", '3.5');
+		return;
+	}
+	
+	return $yarpp->display_related($reference_ID, $args, $echo);
 }
 
-function related_pages($a = array(),$echo=true,$reference_ID=false) {
-	return yarpp_related(array('page'),$a,$echo,$reference_ID);
+function yarpp_related_exist($args = array(), $reference_ID = false) {
+	global $yarpp;
+
+	if ( is_array($reference_ID) ) {
+		_doing_it_wrong( __FUNCTION__, "This YARPP function now takes \$args first and \$reference_ID second.", '3.5');
+		return;
+	}
+	
+	return $yarpp->related_exist($reference_ID, $args, $echo);
 }
 
-function related_entries($a = array(),$echo=true,$reference_ID=false) {
-	return yarpp_related(array('page','post'),$a,$echo,$reference_ID);
+function yarpp_get_related($args = array(), $reference_ID = false) {
+	global $yarpp;
+	return $yarpp->get_related($reference_ID, $args);
 }
 
-function related_posts_exist($a = array(),$reference_ID=false) {
-	return yarpp_related_exist(array('post'),$a,$reference_ID);
+// Here are the related_WHATEVER functions, as introduced in 1.1
+// Since YARPP 2.1, these functions receive (optionally) one array argument.
+
+function related_posts($args = array(), $reference_ID=false, $echo=true) {
+	if ( false !== $reference_ID && is_bool($reference_ID) ) {
+		_doing_it_wrong( __FUNCTION__, "This YARPP function now takes \$args first and \$reference_ID second.", '3.5');
+		return;
+	}
+
+	global $yarpp;
+	if ( $yarpp->get_option('cross_relate') )
+		$args['post_type'] = $yarpp->get_post_types();
+	else
+		$args['post_type'] = array( 'post' );
+
+	return yarpp_related( $args, $reference_ID, $echo );
 }
 
-function related_pages_exist($a = array(),$reference_ID=false) {
-	return yarpp_related_exist(array('page'),$a,$reference_ID);
+function related_pages($args = array(), $reference_ID=false, $echo=true) {
+	if ( false !== $reference_ID && is_bool($reference_ID) ) {
+		_doing_it_wrong( __FUNCTION__, "This YARPP function now takes \$args first and \$reference_ID second.", '3.5');
+		return;
+	}
+
+	global $yarpp;
+	if ( $yarpp->get_option('cross_relate') )
+		$args['post_type'] = $yarpp->get_post_types();
+	else
+		$args['post_type'] = array( 'page' );
+
+	return yarpp_related( $args, $reference_ID, $echo );
 }
 
-function related_entries_exist($a = array(),$reference_ID=false) {
-	return yarpp_related_exist(array('page','post'),$a,$reference_ID);
+function related_entries($args = array(), $reference_ID=false, $echo=true ) {
+	if ( false !== $reference_ID && is_bool($reference_ID) ) {
+		_doing_it_wrong( __FUNCTION__, "This YARPP function now takes \$args first and \$reference_ID second.", '3.5');
+		return;
+	}
+
+	global $yarpp;
+	$args['post_type'] = $yarpp->get_post_types();
+
+	return yarpp_related( $args, $reference_ID, $echo );
+}
+
+function related_posts_exist($args = array(), $reference_ID=false) {
+	global $yarpp;
+	if ( $yarpp->get_option('cross_relate') )
+		$args['post_type'] = $yarpp->get_post_types();
+	else
+		$args['post_type'] = array( 'post' );
+
+	return yarpp_related_exist( $args, $reference_ID );
+}
+
+function related_pages_exist($args = array(), $reference_ID=false) {
+	global $yarpp;
+	if ( $yarpp->get_option('cross_relate') )
+		$args['post_type'] = $yarpp->get_post_types();
+	else
+		$args['post_type'] = array( 'page' );
+	
+	return yarpp_related_exist( $args, $reference_ID );
+}
+
+function related_entries_exist($args = array(),$reference_ID=false) {
+	global $yarpp;
+	$args['post_type'] = $yarpp->get_post_types();
+
+	return yarpp_related_exist( $args, $reference_ID );
 }
