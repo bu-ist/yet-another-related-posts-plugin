438554351c43a294948c91f434ffd3656db5aadf
jdub233 <tamlin@mac.com>
2016-01-26 18:15:10 -0500

    add initial analyze-duplicates command
    The analyze-duplicates command gets the highest scoring relationships
    from the cache table and performs a quick map-reduce style analysis to
    isolate unique 2-way relationships between posts.  This is useful when
    working with a site that has a lot of duplicated posts that are very
    slightly different from each other.

--- classes/YARPP_Cli.php
+++ classes/YARPP_Cli.php
@@ -55,4 +55,51 @@ class YARPP_CLI extends WP_CLI_Command {
 		$yarpp->cache->flush();
 		WP_CLI::success("all cached items cleared");
 	}
+
+	/**
+	* @subcommand analyze-duplicates
+	*/
+	public function analyze_duplicates() {
+		//high-scoring relatedness can indicate duplicate content
+		//this command gets all of the highest scoring relationships, and isolates the higher scoring entry from it's reciprocal entry
+
+		//get all of the high score associations
+		$score='40';
+
+		global $wpdb;
+		$table = $wpdb->prefix . YARPP_TABLES_RELATED_TABLE;
+		$sql = $wpdb->prepare("SELECT * FROM $table WHERE score > '%f' ORDER BY score DESC", $score);
+		$results = $wpdb->get_results($sql, ARRAY_A);
+
+		//each potential dulicate combo will have 2 entries, one for each post.  we need to collapse the two records into one
+
+		//create a target array and only copy unique combinations into it
+		$target = array();
+
+		//initalize by moving first record to the target array 
+		$target[] = array_shift($results);
+
+		foreach ($results as $rec) {
+			//take the reference_ID from the $rec and filter through the $target array
+			//looking for items in the target array where the inverse of the ID and reference_id match
+			//since the results are sorted by score, this should only retain the higher scoring entry
+
+			$match_exists = false;
+
+			foreach ($target as $check) {
+				if ($rec['reference_ID'] == $check['ID'] && $rec['ID'] == $check['reference_ID']) {
+					$match_exists = true;
+				}
+			}
+			//if such a record exists in target then skip the current rec
+
+			//otherwise copy it to target
+			if (!$match_exists) {$target[] = $rec;}
+		}
+
+		//echo json_encode($results);
+		echo json_encode($target);
+
+	}
+
 }
